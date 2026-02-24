c9f88dfb7865488a1c79139f4fd9e24ed5f1e632
pennuig <elisathe2nd@gmail.com>
2026-02-23 19:32:24 -0500

    add team members + sponsor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,58 @@ export default function Home() {
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="text-2xl font-semibold leading-10">About</h1>
           <p className="text-lg leading-8"> 
-            We are GNCE Mercury, a second year FTC team based in Weston, MA.</p>
+            GNCE (Galactic Narwhal Chicken Effect) Mercury is a second year FTC team based in Weston, MA.</p>
         </div>
-
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="text-2xl font-semibold leading-10">Members</h1>
+          <div className="grid grid-cols-6 gap-6"> 
+            <div className="flex flex-col items-center gap-3">
+              <img className="w-30 h-30 aspect-square object-cover rounded-full" src="img-placeholder.png"></img>
+              <p className="">Zade</p>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+              <img className="w-30 h-30 aspect-square object-cover rounded-full" src="img-placeholder.png"></img>
+              <p className="">Elisa</p>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+              <img className="w-30 h-30 aspect-square object-cover rounded-full" src="img-placeholder.png"></img>
+              <p className="">Andy</p>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+              <img className="w-30 h-30 aspect-square object-cover rounded-full" src="img-placeholder.png"></img>
+              <p className="">Noyee</p>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+              <img className="w-30 h-30 aspect-square object-cover rounded-full" src="img-placeholder.png"></img>
+              <p className="">Nicholas</p>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+              <img className="w-30 h-30 aspect-square object-cover rounded-full" src="img-placeholder.png"></img>
+              <p className="">Ethan</p>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+              <img className="w-30 h-30 aspect-square object-cover rounded-full" src="img-placeholder.png"></img>
+              <p className="">Lucas</p>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+              <img className="w-30 h-30 aspect-square object-cover rounded-full" src="img-placeholder.png"></img>
+              <p className="">Kyle</p>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+              <img className="w-30 h-30 aspect-square object-cover rounded-full" src="img-placeholder.png"></img>
+              <p className="">Amy</p>
+            </div>
+          </div>
+        </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="text-2xl font-semibold leading-10">Sponsors</h1>
-          <p className="text-lg leading-8">Solidworks</p>
-          <p className="text-lg leading-8">more coming soon (hopefully) lol</p>
+          <div className="grid">
+          <img className="h-20 w-60 object-contain" src="solidworks.png"></img>
+          </div>
         </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="text-2xl font-semibold leading-10">Our Robot</h1>
-          <p className="text-lg leading-8">coming soon :p</p>
+          <p className="text-lg leading-8">coming soon :D</p>
         </div>
       </main>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,21 +6,26 @@ const Footer = () => {
     <footer className="p-6">
       <div className="flex justify-between items-center">
         <p>&copy; {new Date().getFullYear()} GNCE Mercury</p>
-        <div className="flex space-x-6">
-          <a
-            className="hover:text-gray-300"
-            href="https://www.instagram.com/gnce_mercury/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaInstagram className="h-6 w-6" />
-          </a>
-        </div>
-        <div className="flex gap-2">
-          Contact Us:
-          <Link href="mailto:gncemercury@gmail.com" className="hover:underline">
-            gncemercury@gmail.com
-          </Link>
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-2">
+            Contact Us:
+            <Link
+              href="mailto:gncemercury@gmail.com"
+              className="hover:underline"
+            >
+              gncemercury@gmail.com
+            </Link>
+          </div>
+          <div className="flex space-x-4 justify-end">
+            <a
+              className="hover:text-gray-300"
+              href="https://www.instagram.com/gnce_mercury/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="h-6 w-6" />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
